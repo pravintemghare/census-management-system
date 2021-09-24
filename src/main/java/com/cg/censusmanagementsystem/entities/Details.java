e14ed000bf7bd3ab986fcdfb594261639b55a287
POOJANETKE <77398829+POOJANETKE@users.noreply.github.com>
2021-09-24 12:22:03 +0530

    Update Details.java

--- src/main/java/com/cg/censusmanagementsystem/entities/Details.java
+++ src/main/java/com/cg/censusmanagementsystem/entities/Details.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "detailinformation")
+@Table(name = "census.detailinformation")
 public class Details {
 
 	@Id
